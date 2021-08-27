2c9e3da4d2e24ed2135040e332172534a8b15312
Kenneth Hsu <kennethhsu@gmail.com>
2021-08-27 10:09:53 -0700

    Exclude absentee column from hashing.

--- tests/test_data_tests.py
+++ tests/test_data_tests.py
@@ -7,8 +7,8 @@ from data_tests import duplicate_entries
 class DuplicateEntriesTest(unittest.TestCase):
     def test_ragged_with_duplicates(self):
         rows = [
-            ["header 1", "votes", "header 3", "early_voting", "election_day", "mail", "provisional"],
-            ["a", "b", "", "d", "e", "f", "g"],
+            ["header 1", "votes", "header 3", "early_voting", "election_day", "mail", "provisional", "absentee"],
+            ["a", "b", "", "d", "e", "f", "g", "h"],
             ["a", "b", "c", "d"],
             ["a", "b", "c", "d"]
         ]
@@ -27,9 +27,9 @@ class DuplicateEntriesTest(unittest.TestCase):
 
     def test_ragged_without_duplicates(self):
         rows = [
-            ["header 1", "votes", "header 3", "early_voting", "election_day", "mail", "provisional"],
-            ["a", "b", "c", "d", "e", "f", "g"],
-            ["a", "b", "cc", "d", "e", "f", "g"],
+            ["header 1", "votes", "header 3", "early_voting", "election_day", "mail", "provisional", "absentee"],
+            ["a", "b", "c", "d", "e", "f", "g", "h"],
+            ["a", "b", "cc", "d", "e", "f", "g", "h"],
             ["a", "b", "c", "d"],
             ["a", "b", "d", "e"],
             ["", "a", "b", "d", "e"],
@@ -45,12 +45,16 @@ class DuplicateEntriesTest(unittest.TestCase):
 
     def test_with_duplicates(self):
         rows = [
-            ["header 1", "header 2", "a_votes_b", "c_votes_d", "early_voting", "election_day", "mail", "provisional"],
-            ["a", "b", "c", "d", "e", "f", "g", "h"],
-            ["d", "e", "f", "g", "e", "f", "g", "h"],
-            ["a", "b", "cc", "d", "ee", "f", "gg", "h"],
-            ["a", "b", "c", "dd", "e", "ff", "g", "hh"],
-            ["d", "ee", "f", "g", "e", "f", "g", "h"]
+            [
+                "header 1", "header 2", "a_votes_b", "c_votes_d", "early_voting", "election_day", "mail",
+                "provisional", "absentee"
+            ],
+            ["a", "b", "c", "d", "e", "f", "g", "h", "i"],
+            ["d", "e", "f", "g", "e", "f", "g", "h", "i"],
+            ["a", "b", "cc", "d", "ee", "f", "gg", "h", "i"],
+            ["a", "b", "c", "dd", "e", "ff", "g", "hh", "i"],
+            ["a", "b", "c", "dd", "e", "ff", "g", "h", "ii"],
+            ["d", "ee", "f", "g", "e", "f", "g", "h", "i"]
         ]
 
         data_test = duplicate_entries.DuplicateEntries(rows[0])
@@ -59,22 +63,26 @@ class DuplicateEntriesTest(unittest.TestCase):
         self.assertFalse(data_test.passed)
 
         failure_message = data_test.get_failure_message()
-        self.assertRegex(failure_message, "2 duplicate entries")
+        self.assertRegex(failure_message, "3 duplicate entries")
         self.assertNotRegex(failure_message, "Row 1.*")
         self.assertRegex(failure_message, "Row 2.*" + re.escape(f"{rows[1]}"))
         self.assertNotRegex(failure_message, "Row 3.*")
         self.assertRegex(failure_message, "Row 4.*" + re.escape(f"{rows[3]}"))
         self.assertRegex(failure_message, "Row 5.*" + re.escape(f"{rows[4]}"))
-        self.assertNotRegex(failure_message, "Row 6.*")
+        self.assertRegex(failure_message, "Row 6.*" + re.escape(f"{rows[5]}"))
+        self.assertNotRegex(failure_message, "Row 7.*")
 
     def test_without_duplicates(self):
         rows = [
-            ["header 1", "header 2", "a_votes_b", "header 5", "early_voting", "election_day", "mail", "provisional"],
-            ["a", "b", "c", "d", "e", "f", "g", "h"],
-            ["ab", "", "c", "d", "e", "f", "g", "h"],
-            ["a", "b", "c", "de", "", "f", "g", "h"],
-            ["", "", "", "", "", "", "", ""],
-            ["", "", "", "", "", "", "", ""]
+            [
+                "header 1", "header 2", "a_votes_b", "header 5", "early_voting", "election_day", "mail",
+                "provisional", "absentee"
+            ],
+            ["a", "b", "c", "d", "e", "f", "g", "h", "i"],
+            ["ab", "", "c", "d", "e", "f", "g", "h", "i"],
+            ["a", "b", "c", "de", "", "f", "g", "h", "i"],
+            ["", "", "", "", "", "", "", "", ""],
+            ["", "", "", "", "", "", "", "", ""]
         ]
 
         data_test = duplicate_entries.DuplicateEntries(rows[0])
